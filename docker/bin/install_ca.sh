4b3b76467a34972e2d463440703a6dcb879accc8
dciangot <diego.ciangottini@cern.ch>
2018-11-26 18:10:35 +0100

    update dodas ca

--- docker/bin/install_ca.sh
+++ docker/bin/install_ca.sh
@@ -3,8 +3,9 @@ yum install -y ca-policy-egi-core ca-policy-lcg
 
 /usr/sbin/fetch-crl -q
 
-wget -O /etc/yum.repos.d/ca_CMS-TTS-CA.repo https://ci.cloud.cnaf.infn.it/job/cnaf-mw-devel-jobs/job/ca_CMS-TTS-CA/job/master/lastSuccessfulBuild/artifact/ca_CMS-TTS-CA.repo
-yum -y install ca_CMS-TTS-CA
+wget -O /etc/yum.repos.d/ca_CMS-TTS-CA.repo https://ci.cloud.cnaf.infn.it/view/dodas/job/ca_DODAS-TTS/job/master/lastSuccessfulBuild/artifact/ca_DODAS-TTS.repo
+yum -y install ca_DODAS-TTS
+
 
 chmod 600 /etc/grid-security/xrd/userkey.pem
 
